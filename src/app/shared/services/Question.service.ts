9bb597244b7a42d879218be478b58e81e7523f91
lanebx <havchikos@gmail.com>
2022-01-14 11:53:05 +0200

    added option answer to question 3

--- src/app/shared/services/Question.service.ts
+++ src/app/shared/services/Question.service.ts
@@ -11,7 +11,7 @@ export class TestService {
   allQuestion: Question[] = [
     { id: 1641889825774, question: 'Question 1', answered: true, answer: 'Answer 1', type: 'Open', date: '2019-01-26T13:51:50.417Z', answerOptions: [], dateOfAnswer: '2019-01-26T13:51:50.417Z' },
     { id: 1641889825775, question: 'Question 2', answered: false, answer: '', type: 'Single', date: '2018-01-26T13:51:50.417Z', answerOptions: ['add 3', 'add 4'], dateOfAnswer: '' },
-    { id: 1641889825776, question: 'Question 3', answered: false, answer: '', type: 'Multiple', date: '2012-01-26T13:51:50.417Z', answerOptions: ['add 1', 'add 2'], dateOfAnswer: '' },
+    { id: 1641889825776, question: 'Question 3', answered: false, answer: '', type: 'Multiple', date: '2012-01-26T13:51:50.417Z', answerOptions: ['add 1', 'add 2', 'add 3'], dateOfAnswer: '' },
     { id: 1641889825777, question: 'Question 4', answered: false, answer: '', type: 'Open', date: '2021-01-26T13:51:50.417Z', answerOptions: [], dateOfAnswer: '' },
     { id: 1641889825778, question: 'Question 5', answered: false, answer: '', type: 'Open', date: '2021-01-26T13:51:50.417Z', answerOptions: [], dateOfAnswer: '' },
   ];
